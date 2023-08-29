03ce96a4252e616b6a4a903bd8ad3b94a9b14e6c
MarufUsmonovdev <142956091+MarufUsmonovdev@users.noreply.github.com>
2023-08-29 17:22:33 +0500

    Upload: Vazifani joylash

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+ 
+System.Console.WriteLine("Enter day of the week: ");
+string week = Console.ReadLine();
+
+
+switch (week)
+{
+    case "Dushanba":
+        System.Console.WriteLine("Monday");
+        break;
+    case "Seshanba":
+        System.Console.WriteLine("Tuesday");
+        break;
+    case "Chorshanba":
+        System.Console.WriteLine("Wednesday");
+        break;
+    case "Payshanba":
+        System.Console.WriteLine("Thursday");
+        break;
+    case "Juma":
+        System.Console.WriteLine("Friday");
+        break;
+    case "Shanba":
+        System.Console.WriteLine("Saturday");
+        break;
+    case "Yakshanba":
+        System.Console.WriteLine("Sunday");
+        break;
+    default:
+        System.Console.WriteLine("Sizda yozuvda xato bor!");
+        break;
+
+}
